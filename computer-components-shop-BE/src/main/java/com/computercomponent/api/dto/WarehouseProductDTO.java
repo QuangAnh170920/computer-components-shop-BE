17f7fcc90a493e901b7e2a0747388ed0f6868c9e
QuangAnh170920 <anhphungquang1792@example.com>
2024-11-18 01:14:59 +0700

    update code

--- computer-components-shop-BE/src/main/java/com/computercomponent/api/dto/WarehouseProductDTO.java
+++ computer-components-shop-BE/src/main/java/com/computercomponent/api/dto/WarehouseProductDTO.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class WarehouseProductDTO {
+    private Long id;
+    private Long warehouseId;
     private Long productId;
-    private Integer quantity;
+    private Integer quantity;  // Số lượng của từng sản phẩm có trong đơn
+    private BigDecimal price; // Số tiền của từng sản phẩm có trong đơn
 }
--- computer-components-shop-BE/src/main/java/com/computercomponent/api/entity/WarehouseProduct.java
+++ computer-components-shop-BE/src/main/java/com/computercomponent/api/entity/WarehouseProduct.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -12,4 +13,5 @@ public class WarehouseProduct extends BaseEntity{
     private Long warehouseId;
     private Long productId;
     private Integer quantity;  // Số lượng của từng sản phẩm có trong đơn
+    private BigDecimal price; // Số tiền của từng sản phẩm có trong đơn
 }
